ef6908bbee9a677210ff87d567190303f1c680a5
Auryx <lilbizb1@gmail.com>
2023-08-04 22:34:44 -0700

    Figured out how to do the fetch without DDOSing the API LMAO

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Category from './components/Category';
 import Points from './components/Points';
 import {useState} from 'react'
 
-
 function App() {
 
   console.log("App check")
@@ -13,22 +12,10 @@ function App() {
   const [question, setQuestion] = useState({
     question: ''
   })
-
-  const fetchQuestion = async () => {
-    // construct url for api call
-    const url = `http://jservice.io/api/random`
-    // make api call
-    const response = await fetch(url)
-    // convert response into javascript object
-    const data = await response.json()
-    // update the state
-    setQuestion(data)
-}
-
   return (
     <div className="App">
       <h1>Our Jeapardy App</h1>
-      <NewQuestion fetchQuestion={fetchQuestion}/>
+      <NewQuestion setQuestion={setQuestion}/>
       <Display question={question}/>
       <Points question={question}/>
       <Category question={question}/>
--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,14 +1,17 @@
-import { useRef } from "react";
-
 function NewQuestion(props){
     console.log("NewQuestion check")
-    const formRef = useRef(null)
-    console.log(formRef)
-
-    
-
-
-    return <button onClick={props.fetchQuestion()}>Click Me</button>
+    const fetchQuestion = async () => {
+        console.log("fetchQuestion")
+        // // make api call
+        const response = await fetch(`http://jservice.io/api/random`)
+        console.log(response)
+        // // convert response into javascript object
+        const data = await response.json()
+        console.log(data)
+        // // update the state
+        props.setQuestion(data)
+    }
+    return <button onClick={fetchQuestion}>Click Me</button>
 }
 
-export default NewQuestion
+export default NewQuestion;
